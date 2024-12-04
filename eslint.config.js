b39b419da3ac5ff277d2db4d6c7d6a3863e10452
Erwin Gronek <erwin.gronek@appsfactory.de>
2024-12-04 12:20:54 +0100

    [ZARS-671][ADD]fix linter

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+const tsParser = require('@typescript-eslint/parser');
+const tsEslintPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsEslintPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsEslintPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    files: ['.eslintrc.js'],
+    ignores: [],
+  },
+];
