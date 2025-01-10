5ac343e776004c451070914d808feb3b044cbb48
Erwin Gronek <erwin.gronek@appsfactory.de>
2025-01-10 11:45:10 +0100

    [ZARS-703][ADD] remove ehtical votum checkbox

---
+++ print-frontend/assets/PrintLayout-CzKauamP.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as r,o as t,r as c}from"./print-UXM7QN5P.js";const n={};function s(_,a){const o=c("router-view");return t(),r(o)}const m=e(n,[["render",s]]);export{m as default};
--- print-frontend/assets/PrintLayout-HPA6R5Wd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as r,o as t,r as c}from"./print-Cg3xz5Fr.js";const n={};function s(_,a){const o=c("router-view");return t(),r(o)}const m=e(n,[["render",s]]);export{m as default};
